8e938945ebb0c9b03619114786cbdd25d12150a2
SuperMary <miao.hnlk@foxmail.com>
2016-04-07 14:48:10 +0800

    custom command line

---
+++ sevenDays/main.js
@@ -0,0 +1,8 @@
+var count = require('./util/count');
+var count2 = require('./util/count');
+
+console.log(count.count());
+console.log(count2.count());
+console.log(count2.count());
+console.log(count2.count());
+console.log(count.count());

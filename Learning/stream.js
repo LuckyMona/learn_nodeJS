7fca025986695d3ddb2b0a5cc702e3eeba882fce
Luckymona Tien <miao.hnlk@foxmail.com>
2016-04-26 22:16:08 +0800

    stream

---
+++ Learning/stream.js
@@ -0,0 +1,29 @@
+var fs = require('fs'),
+	contents;
+
+var rs = fs.createReadStream('stream.js');
+
+rs.on('readable',function(){
+	var str;
+	var d = rs.read();
+	if(d)
+	{
+		if(typeof d == "string"){
+			str = d;
+		} else if (typeof d == "object" && d instanceof Buffer) {
+			str = d.toString('utf8');
+		}
+		if(str){
+			/*if(!contents)
+				contents = d;
+			else*/
+				contents += str;
+		}
+
+	}
+});
+
+rs.on('end',function(){
+	console.log("read in the file contents:");
+	console.log(contents.toString('utf8'));
+});

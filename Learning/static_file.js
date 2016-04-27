99cf3e66c397c4bcfaaec3184cdabd8af82d5fe2
Luckymona Tien <miao.hnlk@foxmail.com>
2016-04-27 23:06:55 +0800

    static_file

--- Learning/static_file.js
+++ Learning/static_file.js
@@ -1,7 +1,8 @@
 var fs  = require('fs');
-
+var http = require('http');
+var path = require('path');
 function handle_request(req, res){
-	if(req.url.method.toLowerCase() == 'get' && req.url.substring(0,9) == '/content/'){
+	if(req.method.toLowerCase() == 'get' && req.url.substring(0,9) == '/content/'){
 		serve_static_file(req.url.substring(9), res);
 	} else {
 		res.writeHead(404, { "Content-Type":"application/json"});
@@ -15,10 +16,34 @@ function handle_request(req, res){
 
 function serve_static_file(path, res){
 	var rs = fs.createReadStream(path);
+	var ct = content_type_for_path(path);
+	res.writeHead('200',ct);
 	rs.on('readable',function(){
 		var d = rs.read();
 		if(d){
-			if(typeof d =="")
+			
+			if(typeof d =="string")
+				res.write(d);
+			else if (typeof d == 'object' && d instanceof Buffer)
+				res.write(d.toString('utf8'));
 		}
 	});
+	rs.on('end',function(){
+		res.end();
+	});
+};
+function content_type_for_path (path){
+	var ext = path.extname(path);
+	switch(ext.toLowerCase()){
+		case '.html': return "text/html";
+		case '.js' : return "text/javascript";
+		case '.css': return "text/css";
+		case '.jpg': case '.jpeg':return "image/jpeg";
+		default: return 'text/plain';
+
+	} 
 }
+
+var s = http.createServer(handle_request);
+s.listen(8080);
+
